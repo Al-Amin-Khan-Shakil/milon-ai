ebc986e950a051b405fde37374c61574148a665e
Al-Amin <alaminkhanshakil@gmail.com>
2025-06-29 12:55:45 +0600

    Add bolt badge to landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -14,6 +14,7 @@ import {
   UserPlus,
   Star
 } from 'lucide-react';
+import BoltBadge from '../assets/white_circle_360x360.png';
 
 export const Landing: React.FC = () => {
   const features = [
@@ -79,9 +80,18 @@ export const Landing: React.FC = () => {
 
   return (
     <div className="min-h-screen relative">
-      <div className='absolute top-20 right-4 md:top-24 md:right-6 xl:right-12 z-50'>
-        <a href="https://bolt.new/" target="_blank" rel="noopener noreferrer" className="cursor-pointer">
-          <img src="src\assets\white_circle_360x360.png" alt="Bolt Badge" className="h-16 w-16 md:h-24 md:w-24" />
+      <div className="absolute top-2 right-4 md:top-32 md:right-6 lg:right-10 xl:top-82 xl:right-14 2xl:top-20 z-50">
+        <a
+          href="https://bolt.new/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="cursor-pointer"
+        >
+          <img
+            src={BoltBadge}
+            alt="Bolt Badge"
+            className="h-16 w-16 md:h-24 md:w-24"
+          />
         </a>
       </div>
       {/* Navigation */}
